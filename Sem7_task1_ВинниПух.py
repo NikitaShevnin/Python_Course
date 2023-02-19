70cfe5f7dfe14286b79a2e3075773a7771faa802
NikitaShevnin <mrborman95@gmail.com>
2023-02-19 21:10:43 +0300

    ExportToGithub

--- Sem7_task1_ВинниПух.py
+++ Sem7_task1_ВинниПух.py
@@ -11,12 +11,13 @@
 # Вывод:
 # Парам пам-пам
 
-print ('Введите стишок: ', end=' ')
+print ('Введите стишок ВинниПуха: ', end=' ')
 line = input().lower()
 lines = line.split()
 
-lst = [sum(x in 'уеыаоэяию' for x in lin)
- for lin in lines]
+print (line)
+
+lst = [sum(x in 'уеыаоэяию' for x in lin) for lin in lines]
 
 if len(set(lst)) == 1 :
     res = "Парам пам-пам"  
